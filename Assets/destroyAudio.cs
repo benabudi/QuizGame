20358de598e7a62a494379ec6d6ab66be8f1f300
unknown <tendoben@gmail.com>
2019-04-18 03:18:32 +0300

    first commit

---
+++ Assets/destroyAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyAudio : MonoBehaviour {
+
+    static destroyAudio instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(transform.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+
+}
